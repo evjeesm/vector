01fb47f2a3bebcf51acd261e82b9d64f14e7441b
evjeesm <jekas.obps@gmail.com>
2024-07-17 16:10:43 +0300

    all neccesary preparations are done by autogen.sh; next step is just run `make check`

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
+# we need to rebuild check framework in mingw:
+git clone https://github.com/libcheck/check.git build-aux/check
+cd build-aux/check
+autoreconf -i || exit 1;
 
- autoreconf -fvi || exit 1;
+./configure CFLAGS='-Wall -Wextra -O3' --enable-subunit=no --host x86_64-w64-mingw32 --prefix=$(pwd)/__inst
+make install
 
+echo "Leaving $(pwd)"; cd -;
 
+autoreconf -fvi || exit 1;
+
+./configure CFLAGS='-Wall -Wextra -O3 -Wno-override-init' CHECK_FLAGS="-I$(pwd)/build-aux/check/__inst/include/" CHECK_LIBS="$(pwd)/build-aux/check/__inst/bin/libcheck-0.dll" --host x86_64-w64-mingw32
+
+./patch-test-driver.sh
--- patch-test-driver.sh
+++ patch-test-driver.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-WINE_PREFIX="WINEPATH=$(pwd)/check/__inst/bin/\;/usr/x86_64-w64-mingw32/lib wine "
+WINE_PREFIX="WINEPATH=\'$(pwd)/check/__inst/bin/\;/usr/x86_64-w64-mingw32/lib\' wine "
 PATTERN="s,\(\"\$@\"\),$WINE_PREFIX\1,g"
 
 sed -i "$PATTERN" build-aux/test-driver
