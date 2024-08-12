19bd2e3dea7f79791f0a1c60fa24cc7e1d0e7d3f
evjeesm <jekas.obps@gmail.com>
2024-08-12 12:43:54 +0300

    Generate svg schemes

--- docs/dir_68267d1309a1af8e8297ef4c3efbcdba.js
+++ docs/dir_68267d1309a1af8e8297ef4c3efbcdba.js
@@ -1,7 +1,5 @@
 var dir_68267d1309a1af8e8297ef4c3efbcdba =
 [
-    [ "memswap.c", "memswap_8c_source.html", null ],
-    [ "memswap.h", "memswap_8h_source.html", null ],
     [ "vector.c", "vector_8c.html", "vector_8c" ],
     [ "vector.h", "vector_8h.html", "vector_8h" ]
 ];
