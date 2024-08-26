6a4d96d33482b316ae324c18cf3c3336acf4a557
evjeesm <jekas.obps@gmail.com>
2024-08-26 19:47:53 +0300

    fixup! typo: aligned_malloc

--- examples/aligned_alloc.c
+++ examples/aligned_alloc.c
@@ -8,7 +8,7 @@
 #define ALIGNED(bytes) &(alloc_t){.alignment = bytes}
 
 #if defined _WIN32 || defined __CYGWIN__
-#define aligned_alloc _aligned_malloc
+#define aligned_alloc(alignment, size) _aligned_malloc(size, alignment)
 #endif
 
 typedef struct alloc
