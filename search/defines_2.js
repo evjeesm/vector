7171cd2f530be6c9113777809ff6be312781cbad
evjeesm <46419856+evjeesm@users.noreply.github.com>
2024-10-04 20:16:29 +0000

    Deploying to gh-pages from @ evjeesm/vector@f7b185188122f73a0e608e88ca6a321ccabcb28b 🚀

--- search/defines_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['vector_5fdefault_5fargs_0',['VECTOR_DEFAULT_ARGS',['../vector_8h.html#a5e34f2d91394e1cfcde918615affbc86',1,'vector.h']]]
-];
--- search/searchdata.js
+++ search/searchdata.js
@@ -8,7 +8,7 @@ var indexSectionsWithContent =
   5: "acfpt",
   6: "v",
   7: "v",
-  8: "atv",
+  8: "tv",
   9: "acelpsuv",
   10: "gv📘"
 };
