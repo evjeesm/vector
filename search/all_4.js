1ecbbeb11b5a3666955e7fb331d0520d46e052aa
evjeesm <evjeesm@users.noreply.github.com>
2025-04-19 18:00:50 +0000

    Deploying to gh-pages from @ evjeesm/vector@e10ab4f091bf37904c75458d8859c20a6b93a6ad 🚀

--- search/all_4.js
+++ search/all_4.js
@@ -3,7 +3,7 @@ var searchData=
   ['element_5fsize_0',['element_size',['../structvector__t.html#ada42e4f54768423800961ae3e815406d',1,'vector_t::element_size'],['../structvector__opts__t.html#a18f1f3e195d1c4a691e232b1fcaec0c1',1,'vector_opts_t::element_size']]],
   ['elements_1',['Elements',['../group__Elements.html',1,'']]],
   ['error_2',['❗Customize resize error',['../Guide.html#resize_error',1,'']]],
-  ['example_3',['Minimal Example',['../Readme.html#autotoc_md9',1,'']]],
+  ['example_3',['Minimal Example',['../Readme.html#autotoc_md11',1,'']]],
   ['existing_20vector_4',['⿻ Clone an existing vector',['../Guide.html#cloning',1,'']]],
   ['ext_5fheader_5fsize_5',['ext_header_size',['../structvector__t.html#ad039a1d8cf3619808dcf2bfd04fc4ded',1,'vector_t::ext_header_size'],['../structvector__opts__t.html#ae5a8b3368fff2365e65d2816f7a57ecd',1,'vector_opts_t::ext_header_size']]],
   ['extended_20header_6',['⚙️ Allocate extended header',['../Guide.html#extended_header',1,'']]],
